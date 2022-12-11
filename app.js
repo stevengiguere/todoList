ec8f8eaf600ebf7905044a89060782fadb87a489
stevengiguere <sgiguere87@gmail.com>
2022-12-11 11:34:28 -0500

    allTasks array can be const, as we only push (and later pop) items in it, refactoring date

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bdParser = require('body-parser');
 const ejs = require('ejs');
 const port = 3000;
 const app = express();
-let allTasks = [];
+
+//We still can push tasks to array, but we can't reassign it.
+const allTasks = [];
 
 //Uses
 app.use(bdParser.urlencoded({
@@ -23,22 +25,16 @@ app.set('view engine', 'ejs');
 //Getting the data from the Front
 app.get('/', (req, res) => {
 
-    const date = new Date();
-    const dateLocal = date.getDay();
-    let quote = "";
-
-    if (dateLocal === 6 || dateLocal === 0) {
-        quote = "Yeah on est la fin d'semaine";
-    } else {
-        quote = "Let's go on se magne pour se trouver une job";
-    }
-
-    res.render('index', {
-        itsToday: quote,
-        newTask: allTasks
+    const date = new Date().toLocaleDateString('fr-CA', {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
     });
 
-    
+    res.render('index', {
+        itsToday: date,
+        newTask: allTasks
+    });
 
 })
 
@@ -49,16 +45,16 @@ app.post('/', (req, res) => {
     //Add a new task to the array
     allTasks.push(req.body.addTaskInput);
 
-    
-
-
     //refresh the page just after pushing the data to the array
     res.redirect('/');
 
 })
 
 
-//every thing that go to the back end on the server, on a loge
+//every thing that go to the back end on the server, on a log
 app.listen(port, () => {
+
+
+
     console.log('currently listening on port : ' + port);
 });
